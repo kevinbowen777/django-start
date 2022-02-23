2a9b59bbc1472026ea1739b2dcc4f07a305dfa71
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-23 14:48:14 -0800

    Add class-based view for homepage

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,5 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
