8b8de7852d934ac0b3b289e096bfac16da359127
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-03 14:55:51 -0700

    Add URL name tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,11 +1,22 @@
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 
-class SimpleTests(SimpleTestCase):
+class HomePageTests(SimpleTestCase):
     def test_home_page_status_code(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
+    def test_home_page_url_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+
+class AboutPageTests(SimpleTestCase):
     def test_about_page_status_code(self):
         response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_url_name(self):
+        response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
