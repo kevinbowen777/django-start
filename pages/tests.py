74e3ba2132a893141c3bacb897b873331e677218
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-03 15:59:19 -0700

    Add setUp method. Remove url_name tests

--- pages/tests.py
+++ pages/tests.py
@@ -3,44 +3,36 @@ from django.urls import reverse
 
 
 class HomePageTests(SimpleTestCase):
-    def test_home_page_status_code(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
 
-    def test_home_page_url_name(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
+    def test_home_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
 
     def test_home_page_template(self):
-        response = self.client.get("/")
-        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(self.response, "home.html")
 
     def test_home_page_contains_correct_html(self):
-        response = self.client.get("/")
-        self.assertContains(response, "Homepage")
+        self.assertContains(self.response, "Homepage")
 
     def test_home_page_does_not_contain_incorrect_html(self):
-        response = self.client.get("/")
-        self.assertNotContains(response, "Not the Homepage")
+        self.assertNotContains(self.response, "Not the Homepage")
 
 
 class AboutPageTests(SimpleTestCase):
-    def test_about_page_status_code(self):
-        response = self.client.get("/about/")
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        url = reverse("about")
+        self.response = self.client.get(url)
 
-    def test_about_page_url_name(self):
-        response = self.client.get(reverse("about"))
-        self.assertEqual(response.status_code, 200)
+    def test_about_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
 
     def test_about_page_template(self):
-        response = self.client.get("/about/")
-        self.assertTemplateUsed(response, "about.html")
+        self.assertTemplateUsed(self.response, "about.html")
 
     def test_about_page_contains_correct_html(self):
-        response = self.client.get("/about/")
-        self.assertContains(response, "About page")
+        self.assertContains(self.response, "About page")
 
     def test_about_page_does_not_contain_incorrect_html(self):
-        response = self.client.get("/about/")
-        self.assertNotContains(response, "Not the About page")
+        self.assertNotContains(self.response, "Not the About page")
