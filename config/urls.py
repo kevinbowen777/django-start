135d0d075a9b56ebfcdb71f4701dc8cfa0cbff85
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-03 17:04:45 -0700

    Add URLs for accounts

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    # Django admin
     path("admin/", admin.site.urls),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local apps
+    path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
 ]
 
