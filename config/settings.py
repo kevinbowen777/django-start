1051d3e7f7553147d816bbd6167c88a17b3e7d23
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-09 12:31:36 -0700

    Change staticfiles_dirs from dict to tuple

--- config/settings.py
+++ config/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+STATICFILES_DIRS = (str(BASE_DIR.joinpath("static")),)
 STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
 STATICFILE_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
