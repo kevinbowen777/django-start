19a8d07a2ef6e938c56d19647beeb718826e3f72
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-20 17:57:25 -0700

    Update UserCreation & UserChange

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,17 +1,34 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.core.exceptions import ValidationError
+
+User = get_user_model()
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ("email", "username")
 
 
 class CustomUserCreationForm(UserCreationForm):
+    error_message = UserCreationForm.error_messages.update(
+        {"duplicate_username": "This username has already been taken."}
+    )
+
     class Meta:
-        model = get_user_model()
+        model = User
         fields = (
             "email",
             "username",
         )
 
+    def clean_username(self):
+        username = self.cleaned_data["username"]
 
-class CustomUserChangeForm(UserChangeForm):
-    class Meta:
-        model = get_user_model()
-        fields = ("email", "username")
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+
+        raise ValidationError(self.error_messages["duplicate_username"])
