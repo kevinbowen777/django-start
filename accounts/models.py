e0d221c9edc3e6307d83e28ec00e11e5aab13fe4
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-20 17:56:07 -0700

    Re-write CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models  # noqa:F401
+from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
+    name = models.CharField("Name of User", blank=True, max_length=255)
+    bio = models.TextField("Bio", blank=True)
+
+    def __str__(self):
+        return self.username
