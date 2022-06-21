b80f57cbf65f5317e75fcf9f6ed3f546accd1199
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-21 15:58:24 -0700

    Install nox for testing. Resolves #23

--- noxfile.py
+++ noxfile.py
@@ -3,5 +3,6 @@ import nox
 
 @nox.session(python=["3.10", "3.9"])
 def test(session):
+    args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    session.run("pytest", *args)
