f45c60732636571892fb5e48a7604132e97ead8d
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-21 11:55:27 -0700

    Add form validation test

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,9 +1,11 @@
 import pytest
 from django.contrib.messages.middleware import MessageMiddleware  # noqa:F401
 from django.contrib.sessions.middleware import SessionMiddleware  # noqa:F401
+from django.http import HttpRequest
 from django.test import RequestFactory
 from django.urls import reverse  # noqa:F401
 
+from ..forms import CustomUserChangeForm
 from ..models import CustomUser
 from ..views import UserRedirectView, UserUpdateView
 
@@ -18,6 +20,9 @@ class TestUserUpdateView:
         fixture db access -- this is a work-in-progress for now:
         https://github.com/pytest-dev/pytest-django/pull/258
     """
+
+    def dummy_get_response(self, request: HttpRequest):
+        return None
 
     def test_get_success_url(
         self, user: CustomUser, request_factory: RequestFactory
@@ -41,26 +46,30 @@ class TestUserUpdateView:
 
         assert view.get_object() == user
 
-    """
     def test_form_valid(
         self, user: CustomUser, request_factory: RequestFactory
     ):
+        view = UserUpdateView()
         form_data = {"name": "John Doe"}
-        request = request_factory.post(
-            reverse("user_update"), form_data
-        )
-        request.username = user
-        session_middleware = SessionMiddleware()
-        session_middleware.process_request(request)
-        msg_middleware = MessageMiddleware()
-        msg_middleware.process_request(request)
+        request = request_factory.post(reverse("user_update"), form_data)
+
+        # Add the session/message middleware to the request
+        SessionMiddleware(self.dummy_get_response).process_request(request)
+        MessageMiddleware(self.dummy_get_response).process_request(request)
+        request.user = user
+
+        view.request = request
+
+        # Initialize the form
+        form = CustomUserChangeForm()
+        form.cleaned_data = {}
+        view.form_valid(form)
 
         response = UserUpdateView.as_view()(request)
         user.refresh_from_db()
 
         assert response.status_code == 302
         assert user.name == form_data["name"]
-    """
 
 
 class TestUserRedirectView:
