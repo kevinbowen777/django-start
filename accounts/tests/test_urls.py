70515414cdb0bef5ef25b263bac1374b242689e0
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-21 14:57:57 -0700

    Add user_detail, update, & redirect

---
+++ accounts/tests/test_urls.py
@@ -0,0 +1,24 @@
+import pytest
+from django.urls import resolve, reverse
+
+from ..models import CustomUser
+
+pytestmark = pytest.mark.django_db
+
+
+def test_detail(user: CustomUser):
+    assert (
+        reverse("user_detail", kwargs={"username": user.username})
+        == f"/accounts/{user.username}/"  # noqa:W503
+    )
+    assert resolve(f"/accounts/{user.username}/").view_name == "user_detail"
+
+
+def test_update():
+    assert reverse("user_update") == "/accounts/~update/"
+    assert resolve("/accounts/~update/").view_name == "user_update"
+
+
+def test_redirect():
+    assert reverse("redirect") == "/accounts/~redirect/"
+    assert resolve("/accounts/~redirect/").view_name == "redirect"
