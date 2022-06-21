4317bcd9a44b625c285e8812e71a7c572232926b
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-20 19:19:35 -0700

    Add test_assert for CustomUser

--- accounts/tests.py
+++ accounts/tests.py
@@ -6,41 +6,64 @@ from .views import SignupPageView
 
 
 class CustomUserTests(TestCase):
-    def test_create_user(self):
+    def setUp(self):
         User = get_user_model()
-        user = User.objects.create_user(
+        self.user = User.objects.create_user(
             username="kevin",
             email="kevin@example.com",
             password="T3stP@5s123",
         )
-        self.assertEqual(user.username, "kevin")
-        self.assertEqual(user.email, "kevin@example.com")
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
 
-    def test_create_superuser(self):
-        User = get_user_model()
-        user = User.objects.create_superuser(
+        self.super_user = User.objects.create_superuser(
             username="superadmin",
             email="superadmin@example.com",
             password="T3stP@5s123",
         )
-        self.assertEqual(user.username, "superadmin")
-        self.assertEqual(user.email, "superadmin@example.com")
-        self.assertTrue(user.is_active)
-        self.assertTrue(user.is_staff)
-        self.assertTrue(user.is_superuser)
+
+    def test_create_user(self):
+        self.assertEqual(self.user.username, "kevin")
+        self.assertEqual(self.user.email, "kevin@example.com")
+        self.assertTrue(self.user.is_active)
+        self.assertFalse(self.user.is_staff)
+        self.assertFalse(self.user.is_superuser)
+
+    def test_user_asserts(self):
+        User = get_user_model()
+        try:
+            self.assertIsNotNone(self.user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email="")
+        with self.assertRaises(ValueError):
+            User.objects.create_user(username="", email="", password="foo")
+
+    def test_create_superuser(self):
+        self.assertEqual(self.super_user.username, "superadmin")
+        self.assertEqual(self.super_user.email, "superadmin@example.com")
+        self.assertTrue(self.super_user.is_active)
+        self.assertTrue(self.super_user.is_staff)
+        self.assertTrue(self.super_user.is_superuser)
+
+    def test_superuser_asserts(self):
+        User = get_user_model()
+        try:
+            self.assertIsNotNone(self.super_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                username="",
+                email="super@user.com",
+                password="foo",
+                is_superuser=False,
+            )
 
     def test___str__(self):
-        User = get_user_model()
-        user = User.objects.create_user(
-            username="kevin",
-            email="kevin@example.com",
-            password="T3stP@5s123",
-        )
-        assert user.__str__() == user.username
-        assert str(user) == user.username
+        assert self.user.__str__() == self.user.username
+        assert str(self.user) == self.user.username
 
 
 class SignupPageTests(TestCase):
