b352f750af115b7cd288fb2932437c734c44e387
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-20 20:20:28 -0700

    Move CustomUser to test_models.py

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,8 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import resolve, reverse
-
-from .views import SignupPageView
 
 
 class CustomUserTests(TestCase):
@@ -64,38 +61,3 @@ class CustomUserTests(TestCase):
     def test___str__(self):
         assert self.user.__str__() == self.user.username
         assert str(self.user) == self.user.username
-
-
-class SignupPageTests(TestCase):
-
-    username = "newuser"
-    email = "newuser@example.com"
-
-    def setUp(self):
-        url = reverse("account_signup")
-        self.response = self.client.get(url)
-
-    def test_signup_template(self):
-        self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, "account/signup.html")
-        self.assertContains(self.response, "Sign Up")
-        self.assertNotContains(
-            self.response, "Hi there! I should not be on the page."
-        )
-
-    def test_signup_form(self):
-        new_user = get_user_model().objects.create_user(  # noqa: F841
-            self.username, self.email
-        )
-        self.assertEqual(get_user_model().objects.all().count(), 1)
-        self.assertEqual(
-            get_user_model().objects.all()[0].username, self.username
-        )
-        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
-
-    def test_signup_view(self):
-        view = resolve("/accounts/signup/")
-        self.assertEqual(
-            view.func.__name__,
-            SignupPageView.as_view().__name__,
-        )
