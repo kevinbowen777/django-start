52b1ad2baec12aeb663d3779faa404ef3fb4828f
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-21 17:25:05 -0700

    Add black as a session

--- noxfile.py
+++ noxfile.py
@@ -15,3 +15,10 @@ def lint(session):
     args = session.posargs or locations
     session.install("flake8")
     session.run("flake8", *args)
+
+
+@nox.session(python=["3.10", "3.9"])
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
