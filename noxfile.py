ac185a4e99bf9552b839c7dcb38b5e0618abe1e7
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-21 17:05:45 -0700

    Add flake8 session. Resolves #24

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,17 @@
 import nox
 
+locations = "accounts", "config", "pages", "noxfile.py"
+
 
 @nox.session(python=["3.10", "3.9"])
 def test(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+@nox.session(python=["3.10", "3.9"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
