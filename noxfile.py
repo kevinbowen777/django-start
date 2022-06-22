ff7c1a76f3d9ff3ae45db680b771fd39ee736f33
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-22 16:59:42 -0700

    Add flake8-docstrings to lint session

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,4 @@
+"""Nox sessions."""
 import tempfile
 
 import nox
@@ -7,6 +8,7 @@ locations = "accounts", "config", "pages", "noxfile.py"
 
 
 def install_with_constraints(session, *args, **kwargs):
+    """Install packages constrained by Poetry's lock file."""
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -21,7 +23,32 @@ def install_with_constraints(session, *args, **kwargs):
 
 
 @nox.session(python=["3.10", "3.9"])
+def black(session):
+    """Run black code formatter."""
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
+
+
+@nox.session(python=["3.10", "3.9"])
+def lint(session):
+    """Lint using flake8."""
+    args = session.posargs or locations
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-docstrings",
+        "flake8-import-order",
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.10", "3.9"])
 def safety(session):
+    """Scan dependencies for insecure packages."""
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -40,6 +67,7 @@ def safety(session):
 
 @nox.session(python=["3.10", "3.9"])
 def tests(session):
+    """Run the test suite."""
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(
@@ -52,24 +80,3 @@ def tests(session):
         "pytest-django",
     )
     session.run("pytest", *args)
-
-
-@nox.session(python=["3.10", "3.9"])
-def lint(session):
-    args = session.posargs or locations
-    install_with_constraints(
-        session,
-        "flake8",
-        "flake8-bandit",
-        "flake8-black",
-        "flake8-bugbear",
-        "flake8-import-order",
-    )
-    session.run("flake8", *args)
-
-
-@nox.session(python=["3.10", "3.9"])
-def black(session):
-    args = session.posargs or locations
-    install_with_constraints(session, "black")
-    session.run("black", *args)
