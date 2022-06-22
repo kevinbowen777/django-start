d7e92f9c083193ded0fd5e456a6a89048711d1db
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-21 18:09:34 -0700

    Add flake8-import-order to session

--- noxfile.py
+++ noxfile.py
@@ -14,7 +14,7 @@ def tests(session):
 @nox.session(python=["3.10", "3.9"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black")
+    session.install("flake8", "flake8-black", "flake8-import-order")
     session.run("flake8", *args)
 
 
