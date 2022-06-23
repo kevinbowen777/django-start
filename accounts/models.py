17066a4056193c6a6d1793d441a408f6000652f8
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-22 17:46:28 -0700

    Add docstrings

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,11 @@ from django_countries.fields import CountryField
 
 
 class CustomUser(AbstractUser):
+    """Default custom user model for django-start template project.
+    If adding fields that need to be filled at user signup,
+    check forms.SignupForm and forms.SocialSignupForms accordingly.
+    """
+
     name = models.CharField("Name of User", blank=True, max_length=255)
     age = models.PositiveIntegerField(null=True, blank=True)
     bio = models.TextField("Bio", blank=True)
@@ -15,4 +20,10 @@ class CustomUser(AbstractUser):
         return self.username
 
     def get_absolute_url(self):
+        """Get URL for user's detail view.
+
+        Returns:
+            str: URL for user detail.
+
+        """
         return reverse("user_detail", kwargs={"username": self.username})
