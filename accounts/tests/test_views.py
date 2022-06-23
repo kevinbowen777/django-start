5b22ec3255b879ddfd8109bac09d436ece77018a
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-22 17:40:15 -0700

    Modify docstring config

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -13,12 +13,12 @@ pytestmark = pytest.mark.django_db
 
 
 class TestUserUpdateView:
-    """
-    TODO:
-        extracting view initialization code as class-scoped fixture
-        would be great if only pytest-django supported non-function-scoped
-        fixture db access -- this is a work-in-progress for now:
-        https://github.com/pytest-dev/pytest-django/pull/258
+    """TODO:
+
+    extracting view initialization code as class-scoped fixture
+    would be great if only pytest-django supported non-function-scoped
+    fixture db access -- this is a work-in-progress for now:
+    https://github.com/pytest-dev/pytest-django/pull/258
     """
 
     def dummy_get_response(self, request: HttpRequest):
