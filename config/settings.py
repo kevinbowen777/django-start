5eafc8c52ec351ff63214ca6e811f1e944c194f8
Kevin Bowen <kevin.bowen@gmail.com>
2022-09-15 18:39:39 -0700

    Implement base logging template. Resolves #79

--- config/settings.py
+++ config/settings.py
@@ -155,6 +155,49 @@ EMAIL_USE_TLS = True
 # django-crispy-forms
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
+ADMINS = [("Kevin Bowen", "kevinbowen@protonmail.com")]
+MANAGERS = ADMINS
+
+# LOGGING
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}
+    },
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+        "django.security.DisallowedHost": {
+            "level": "ERROR",
+            "handlers": ["console", "mail_admins"],
+            "propagate": True,
+        },
+    },
+}
+
 # django-debug-toolbar
 # Note: disable when running Nox tests
 # Use the following in Docker only:
