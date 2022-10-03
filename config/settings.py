ff352bdf5331ebeb8c6158c47cba5916176f1926
kevinbowen777 <kevin.bowen@gmail.com>
2022-10-03 11:05:07 -0700

    Add BrokenLinkEmailMiddleware. Fixes #84

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
