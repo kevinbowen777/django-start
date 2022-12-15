0bf6f5f0456959ce78189e71e8c27e2c41c7bfe5
Kevin Bowen <kevin.bowen@gmail.com>
2022-12-14 19:49:47 -0800

    Add sample page list

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import AboutPageView, HomePageView
+from .views import AboutPageView, HomePageView, SamplePageView
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
+    path("sample1/", SamplePageView.as_view(), name="sample"),
     path("", HomePageView.as_view(), name="home"),
 ]
