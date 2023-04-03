9b036fa8a20262f33653519478941c3c209a9aae
Kevin Bowen <kevin.bowen@gmail.com>
2023-04-02 20:08:22 -0700

    Re-write factory password gen using PostGenerationMethodCall. Resolves #94

--- accounts/tests/factories.py
+++ accounts/tests/factories.py
@@ -1,8 +1,6 @@
-from typing import Any, Sequence
-
 from django.contrib.auth import get_user_model
 
-from factory import Faker, post_generation
+from factory import Faker, PostGenerationMethodCall
 from factory.django import DjangoModelFactory
 
 
@@ -10,7 +8,12 @@ class UserFactory(DjangoModelFactory):
     username = Faker("user_name")
     email = Faker("email")
     name = Faker("name")
+    # This allows us to use the latest factory-boy package; but, for now, need
+    # to hard-code the password.
+    # See: https://github.com/FactoryBoy/factory_boy/issues/359
+    password = PostGenerationMethodCall("set_password", "P@s5word")
 
+    """
     @post_generation
     def password(self, create: bool, extracted: Sequence[Any], **kwargs):
         password = (
@@ -26,6 +29,7 @@ class UserFactory(DjangoModelFactory):
             ).generate(extra_kwargs={})
         )
         self.set_password(password)
+        """
 
     class Meta:
         model = get_user_model()
