0e0c25a09805cbd1e3efaf5530199234ab8a00af
Kevin Bowen <kevin.bowen@gmail.com>
2023-06-27 16:20:13 -0700

    Add support for running dev server with HTTPS. Resolves #235

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["django-start.cool", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -195,9 +195,7 @@ MANAGERS = ADMINS
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    "filters": {
-        "require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}
-    },
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
     "formatters": {
         "verbose": {
             "format": "%(levelname)s %(asctime)s %(module)s "
