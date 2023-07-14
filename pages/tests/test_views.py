7fa152ed59e6aca88aa2dd3a1f71231a7d49e498
Kevin Bowen <kevin.bowen@gmail.com>
2023-07-13 17:54:23 -0700

    Fix pages tests. 100% coverage

--- pages/tests/test_views.py
+++ pages/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.core.mail import BadHeaderError
 from django.test import SimpleTestCase
 from django.urls import resolve, reverse
 
@@ -83,6 +84,22 @@ class ContactViewTests(SimpleTestCase):
             view.func.__name__,
             ContactView.__name__,
         )
+
+    def test_header_injection(self):
+        error_occured = True
+        try:
+            self.client.post(
+                "/contact/",
+                data={
+                    "from_email": "joe@example.com",
+                    "subject": "Subject\nInjectionTest",
+                    "message": "This is a test of a BadHeaderError",
+                },
+            )
+            error_occured = False
+        except BadHeaderError:
+            error_occured = True
+        self.assertFalse(error_occured)
 
 
 class SampleViewTests(SimpleTestCase):
