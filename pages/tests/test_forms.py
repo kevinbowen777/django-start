c04c96b7a12e5c0cc8b311c5a1fcf1482a844267
Kevin Bowen <kevin.bowen@gmail.com>
2023-08-06 18:13:13 -0700

    Update coverage report config(#265)

--- pages/tests/test_forms.py
+++ pages/tests/test_forms.py
@@ -1,18 +1,8 @@
 from django.core.mail import BadHeaderError
 from django.test import SimpleTestCase
-from django.urls import reverse
 
 
 class ContactFormTests(SimpleTestCase):
-    def setUp(self):
-        url = reverse("contact")
-        self.response = self.client.get(url)
-        self.form_data = {
-            "from_email": "joe@example.com",
-            "subject": "Test Email",
-            "message": "This is a test email",
-        }
-
     def test_contact_page_form_is_valid(self):
         response = self.client.post(
             "/contact/",
@@ -23,6 +13,17 @@ class ContactFormTests(SimpleTestCase):
             },
         )
         self.assertEqual(response.status_code, 302)
+
+    def test_contact_form_is_invalid(self):
+        response = self.client.post(
+            "/contact/",
+            data={
+                "from_email": "john@example.com",
+                "subject": "Test Email",
+                "not_a_form_field": "This is a test",
+            },
+        )
+        self.assertEqual(response.status_code, 200)
 
     def test_header_injection(self):
         error_occured = True
