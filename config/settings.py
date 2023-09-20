294f897f2cc0f0424df8f11b4059831c3fead8f4
Kevin Bowen <kevin.bowen@gmail.com>
2023-09-20 14:44:54 -0700

    Update django-allauth. Add AccountMiddleware. Resolves #287 (#306)

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "allauth.account.middleware.AccountMiddleware",
     "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
