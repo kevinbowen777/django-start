ac02783a0416efdc64fde9088fa40766e5b90727
Kevin Bowen <kevin.bowen@gmail.com>
2023-09-26 17:15:15 -0700

    Add OAuth config. Add page tests. Resolves #185

--- config/settings.py
+++ config/settings.py
@@ -160,6 +160,20 @@ ACCOUNT_EMAIL_VERIFICATION = "optional"
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = True
 ACCOUNT_UNIQUE_EMAIL = True
 
+SOCIALACCOUNT_PROVIDERS = {
+    "github": {
+        # For each OAuth based provider, either add a ``SocialApp``
+        # (``socialaccount`` app) containing the required client
+        # credentials, or list them here:
+        # "VERIFIED_EMAIL": True
+        "APP": {
+            "client_id": env.str("GITHUB_SOCIALACCOUNT_APP_CLIENT_ID"),
+            "secret": env.str("GITHUB_SOCIALACCOUNT_APP_SECRET"),
+            "key": "",
+        },
+    }
+}
+
 # third-party email relay configuration
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
