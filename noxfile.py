110089c2af56503d41b8a629b45880c465419edd
Kevin Bowen <kevin.bowen@gmail.com>
2026-08-04 12:41:47 -0700

    Upgrade Django to 6.0.8. Resolves #636 (#637)

--- noxfile.py
+++ noxfile.py
@@ -117,6 +117,7 @@ def tests(session):
     session.run(
         "python",
         "-Wonce::DeprecationWarning",
+        # "-Walways::DeprecationWarning",
         "-Im",
         "pytest",
         *args,
